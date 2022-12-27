c366f3d9c6b10776d3149a434a2ab1422af2d62c
Cinthya <cinthyadasilvasoares@outlook.com>
2022-12-27 11:06:19 -0300

    arrays/type cast/ criando conta que substitui array

--- ByteBankHerdadoConta/src/br/com/bytebank/teste/TesteArrayReferencias.java
+++ ByteBankHerdadoConta/src/br/com/bytebank/teste/TesteArrayReferencias.java
@@ -1,22 +1,32 @@
 package br.com.bytebank.teste;
 
+import br.com.bytebank.modelo.Cliente;
+import br.com.bytebank.modelo.Conta;
 import br.com.bytebank.modelo.ContaCorrente;
+import br.com.bytebank.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 	
 	public static void main(String[] args) {
 		
 		//int[] idades = new int[5];
-		ContaCorrente [] contas = new ContaCorrente [5];
+		Object [] referencias = new Object [5];//nasce e morre do mesmo tamanho
 		
 		ContaCorrente cc1 = new ContaCorrente(11,22);		
-		contas [0] = cc1;
+		referencias [0] = cc1;
 		
-		ContaCorrente cc2 = new ContaCorrente(33,11);
-		contas [2] = cc2;
+		ContaPoupanca cp1 = new ContaPoupanca(33,44);
+		referencias [1] = cp1;
+		
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
+		
+//		ContaPoupanca ref = (ContaPoupanca) referencias[2]; //type cast 
+//		System.out.println(ref.getNumero());
 		
 		System.out.println(cc1);
-		System.out.println(cc2);
+		System.out.println(cp1);
+		System.out.println(cliente);
 		
 	}
 
---
+++ ByteBankHerdadoConta/src/br/com/bytebank/teste/TesteGuardarRef.java
@@ -0,0 +1,25 @@
+package br.com.bytebank.teste;
+
+import br.com.bytebank.modelo.Conta;
+import br.com.bytebank.modelo.ContaCorrente;
+import br.com.bytebank.modelo.GuardarReferencias;
+
+public class TesteGuardarRef {
+	
+	public static void main(String[] args) {
+		GuardarReferencias guardando = new GuardarReferencias();
+		
+		Conta cc = new ContaCorrente(55,66);
+		guardando.adiciona(cc);
+		
+		Conta cc2 = new ContaCorrente(77,88);
+		guardando.adiciona(cc2);
+		
+		int tamanho = guardando.getQuantidadeDeElementos();
+		System.out.println("Posições livres: " + tamanho);
+		
+		Conta ref = guardando.getReferencia(0);
+		System.out.println(ref.getNumero());
+	}
+
+}
